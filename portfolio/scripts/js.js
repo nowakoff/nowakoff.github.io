3aa94645457566c820c76c0066809adf97102cbc
nowakoff <anowak@icstars.org>
2016-02-19 14:37:09 +0100

    Add resume page

--- portfolio/scripts/js.js
+++ portfolio/scripts/js.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 	// alert('wwww1');
-    $("p").click(function(){
-        $(this).hide();
+    $("#ic").click(function(){
+        $('#icHide').show();
+        $('#acHide').hide();
+        $('#deHide').hide();
+    });
+    $("#ac").click(function(){
+        $('#icHide').hide();
+        $('#acHide').show();
+        $('#deHide').hide();
+    });
+    $("#de").click(function(){
+        $('#icHide').hide();
+        $('#acHide').hide();
+        $('#deHide').show();
     });
 });
